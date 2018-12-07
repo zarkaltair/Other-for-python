ec708e294eaaac5d74e28cbb43cbb4d09093734f
Zark <zarkaltair@gmail.com>
2018-12-07 22:17:58 +0500

    Add new task

--- test for codewars.py
+++ test for codewars.py
@@ -523,15 +523,22 @@
 # print(x)
 
 
-def quicksum(packet):
-    alphabet = {' ': 0, 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
-    sum = 0
-    for i in packet:
-        sum += (packet.index(i) + 1) * alphabet[i]
-        print(packet)
-        packet = packet.replace(i, ' ', 1)
-        print(packet)
-        print(sum)
-    return sum
+# def quicksum(packet):
+#     alphabet = {' ': 0, 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+#     sum = 0
+#     for i in packet:
+#         sum += (packet.index(i) + 1) * alphabet[i]
+#         print(packet)
+#         packet = packet.replace(i, ' ', 1)
+#         print(packet)
+#         print(sum)
+#     return sum
 
-print(quicksum("BBC"))
+# print(quicksum("BBC"))
+
+
+def mix(s1, s2):
+    return s1, s2
+print(mix("A aaaa bb c", "& aaa bbb c d"))    
+
+# "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
