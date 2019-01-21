34cfba83c395a56a35a08658d3eb1ce9ddf17fa1
zarkaltair <zarkaltair@gmail.com>
2019-01-21 21:54:49 +0500

    Requests
    Requests without ssl

--- moduls for python.py
+++ moduls for python.py
@@ -1,26 +1,38 @@
-import os
-import time
-from sys import version
+# import os
+# import time
+# from sys import version
 
 
-print(os.getcwd())
-print()
-print(time.localtime())
-print()
-print(version)
-print()
-print(dir(time))
-print()
+# print(os.getcwd())
+# print()
+# print(time.localtime())
+# print()
+# print(version)
+# print()
+# print(dir(time))
+# print()
+
+
+
+# import requests
+
+
+# check internet connection
+# def internetConnection():
+# 	try:
+# 		requests.get('http://google.com', timeout=3)
+# 		print('connected')
+# 	except requests.exceptions.ConnectionError:
+# 		print('not connected')
+		
+# internetConnection()
+
 
 
 import requests
 
 
-def internetConnection():
-	try:
-		requests.get('http://google.com', timeout=3)
-		print('connected')
-	except requests.exceptions.ConnectionError:
-		print('not connected')
-		
-internetConnection()
+# отключение SSL
+url = 'https://free-proxy-list.net/'
+response = requests.get(url, verify=False)
+print(response)
