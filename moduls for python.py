47de530ee5e477ecf1171068110c34c58fc46a7e
zarkaltair <zarkaltair@gmail.com>
2019-01-24 00:27:47 +0500

    Replace some files
    Replace some files

--- moduls for python.py
+++ moduls for python.py
@@ -11,28 +11,3 @@
 # print()
 # print(dir(time))
 # print()
-
-
-
-# import requests
-
-
-# check internet connection
-# def internetConnection():
-# 	try:
-# 		requests.get('http://google.com', timeout=3)
-# 		print('connected')
-# 	except requests.exceptions.ConnectionError:
-# 		print('not connected')
-		
-# internetConnection()
-
-
-
-import requests
-
-
-# отключение SSL
-url = 'https://free-proxy-list.net/'
-response = requests.get(url, verify=False)
-print(response)
