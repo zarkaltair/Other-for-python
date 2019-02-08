e3c0b53993cb30ee256595224e0762df547ce4dd
Zark <zarkaltair@gmail.com>
2019-02-08 16:30:42 +0500

    test chat server client

---
+++ My_projects/Test_chat/server.py
@@ -0,0 +1,35 @@
+import socket
+import time
+
+
+host = socket.gethostbyname(socket.gethostname())
+port = 9090
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+
+quit = False
+print('Server Started - ' + host + ':' + str(port))
+
+while not quit:
+	try:
+		data, addr = s.recvfrom(1024)
+
+		if addr not in clients:
+			clients.append(addr)
+
+		itsatime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+		print("[" + addr[0] + "] - [" + str(addr[1]) + "] - [" + itsatime +"]/", end="")
+		print(data.decode("utf-8"))
+
+		for client in clients:
+			if addr != client:
+				s.sendto(data, client)
+	except:
+		print("\nServer Stopped")
+		quit = True
+
+s.close()
